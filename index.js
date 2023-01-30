cb7e4dfb98cecd102363501a29bcbc0d02d20a9f
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-01-30 16:33:24 +0100

    setup heroku

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {supabase} from "./supabaseClient.js";
-import {fetchAllBillboards} from "./src/utils/parsers.js";
+import {fetchAllBillboards, fetchBillboardByYear} from "./src/utils/parsers.js";
 import express from "express";
 const app = express();
 const PORT = 1992;
@@ -11,10 +11,24 @@ app.listen(
 
 const billboards = await fetchAllBillboards()
 
-app.get('/exhibitions/billboardseries', (req, res) => {
+// retrieve all billboards;
+app.get('/exhibitions/billboardseries/', (req, res) => {
+    var _test = req.query.test;
+    if (req.query.test){
+        const billboards = [];
+        for (let x=0; x < billboards.length; x++) {
+            if (billboards[x]["GecureerdeCollectie.bestaatUit"]["MensgemaaktObject.titel"].includes(_test)){
+                console.log(billboards[x]);
+                billboards.push(billboards[x]);
+            }
+        }
+    }
+
     res.send(
         {
             billboards
         }
     );
 })
+
+// retrieve billboards for a specific year;
--- src/utils/parsers.js
+++ src/utils/parsers.js
@@ -10,3 +10,15 @@ export async function fetchAllBillboards() {
     }
     return billboards;
 }
+
+export async function fetchBillboardByYear(year) {
+    const {data, error} = await supabase
+        .from("exh_billboardseries")
+        .select("OSLO", {'head':false})
+        .like("date_start", year)
+    let billboards = []
+    for (let x=0; x < data.length; x++) {
+        billboards.push(data[x]["OSLO"])
+    }
+    return billboards;
+}

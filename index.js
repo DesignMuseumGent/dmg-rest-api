e964fc292f444d4070e15f88c3fb56b5ce0c88a3
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-03-13 11:29:21 +0100

    add ARK:Naan

--- index.js
+++ index.js
@@ -38,11 +38,36 @@ async function start(){
         );
     })
 
+    // ark
+    app.get('/ark:/29417/exhibitions/billboardseries/', (req, res) => {
+        const billboards = [];
+        for (let x=0; x < billboard.length; x++) {
+            if (billboard[x]){
+                console.log(billboard[x]);
+                billboards.push(billboard[x]);
+            }
+        }
+
+        res.send(
+            {
+                billboards
+            }
+        );
+    })
+
     app.get('/agents/:agentPID', async(req, res) => {
         const x = await fetchLDESRecordByAgentID(req.params.agentPID);
         const result_cidoc = x[0]["LDES_raw"];
         res.send({result_cidoc})
     })
+
+    // ark
+    app.get('/ark:/29417/agents/:agentPID', async(req, res) => {
+        const x = await fetchLDESRecordByAgentID(req.params.agentPID);
+        const result_cidoc = x[0]["LDES_raw"];
+        res.send({result_cidoc})
+    })
+
 
     app.get('/exhibitions/:exhibitionPID', async (req, res)=>{
         const x = await fetchLDESrecordsByExhibitionID(req.params.exhibitionPID)
@@ -56,7 +81,19 @@ async function start(){
 
     } )
 
-    app.get('/objects/:objectNumber', async (req, res) => {
+    app.get('/ark:/29417/exhibitions/:exhibitionPID', async (req, res)=>{
+        const x = await fetchLDESrecordsByExhibitionID(req.params.exhibitionPID)
+        try{
+            const result_cidoc = x[0]["LDES_raw"];
+            console.log(result_cidoc)
+            res.send({result_cidoc})
+        } catch (e) {
+            console.log(e)
+        }
+
+    } )
+
+    app.get('/objects/:objectNumber' || '/ark:/29417/objects/:objectNumber', async (req, res) => {
 
         const x = await fetchLDESRecordByObjectNumber(req.params.objectNumber)
 
@@ -86,6 +123,39 @@ async function start(){
 
 
     })
+
+    // ark
+    app.get('/ark:/29417/objects/:objectNumber', async (req, res) => {
+
+        const x = await fetchLDESRecordByObjectNumber(req.params.objectNumber)
+
+        const result_cidoc = x[0]["LDES_raw"];
+        const result_oslo = x[0]["OSLO"];
+
+        const type = req.params.ikwil;
+        console.log(req.params.ikwil);
+
+        if(type === "OSLO") {
+            res.send(
+                {result_oslo}
+            )
+        }
+
+        else if(type === "CIDOC") {
+            res.send(
+                {result_cidoc}
+            )
+        }
+
+        else {
+            res.send(
+                {result_cidoc}
+            )
+        }
+
+
+    })
+
     console.log("DONE :D :D :D :D ")
 }
 

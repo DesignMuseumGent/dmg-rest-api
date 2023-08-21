5fd81ea9d6dd4c531d70b46f14ad55394779bab0
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-08-21 14:10:57 +0200

    restructure index.js (clean code) - move routes to route folder and import as definitions.

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ import cors from 'cors'
 // import functions to populate API.
 import {
     fetchAllBillboards,
-    fetchLDESRecordByAgentID,
-    fetchLDESrecordsByExhibitionID,
-    fetchAllExhibitions,
-    fetchTexts
 } from "./src/utils/parsers.js";
 
 // import routes (API contructors)
 import {requestObjects, requestObject} from "./src/routes/objects.js";
 import {requestDCAT} from "./src/routes/dcat.js";
 import {requestAllBillboards} from "./src/routes/billboards.js";
+import {requestAgents} from "./src/routes/agents.js";
+import {requestExhibition, requestExhibitions} from "./src/routes/exhibitions.js";
+import {requestTexts} from "./src/routes/texts.js";
 
 async function start(){
 
@@ -48,121 +47,14 @@ async function start(){
     // ROUTES to exhibition data
     const billboard = await fetchAllBillboards() // fetch data from supabase
     requestAllBillboards(app, billboard); // request all billboards. (endpoint)
+    requestExhibition(app); // request all exhibitions
+    requestExhibitions(app); // request single exhibition
 
-    // *** AGENTS *** //
-    app.get('/id/agents/:agentPID', async(req, res) => {
-        const x = await fetchLDESRecordByAgentID(req.params.agentPID);
-        const result_cidoc = x[0]["LDES_raw"];
-        res.send({result_cidoc})
-    })
+    // ROUTES to agent (authority list) data
+    requestAgents(app)
 
-    // ark
-    app.get('/id/ark:/29417/agents/:agentPID', async(req, res) => {
-        const x = await fetchLDESRecordByAgentID(req.params.agentPID);
-        const result_cidoc = x[0]["LDES_raw"];
-        res.send({result_cidoc})
-    })
-
-    // *** EXHIBITIONS *** //
-    app.get('/id/exhibitions', async(req, res)=> {
-        const exh = await fetchAllExhibitions()
-        let range = exh.length
-        let _exhibitions = []
-
-        for (let i = 0; i < range; i ++) {
-            let _exhibition = {}
-            _exhibition["@id"] = baseURI+"exhibition/"+exh[i]["exh_PID"]
-            _exhibition["cidoc:P1_is_identified_by"] = {
-                "@type": "cidoc:E33_E41_Linguistic_Appellation",
-                "inhoud": {
-                    "@value": exh[i]["LDES_raw"]["object"]["cidoc:P1_is_identified_by"]["inhoud"]["@value"],
-                    "@language": "nl"
-                }
-            }
-            _exhibitions.push(_exhibition)
-        }
-        res.send({_exhibitions})
-    })
-
-    app.get('/id/exhibition/:exhibitionPID', async (req, res)=> {
-        const x = await fetchLDESrecordsByExhibitionID(req.params.exhibitionPID)
-        try{
-            console.log(x[0]["LDES_raw"])
-            res.send(x[0]["LDES_raw"])
-        } catch (e) {
-            console.log(e)
-        }
-    })
-
-    app.get('/id/ark:/29417/exhibition/:exhibitionPID', async (req, res)=> {
-        const x = await fetchLDESrecordsByExhibitionID(req.params.exhibitionPID)
-        try{
-            const result_cidoc = x[0]["LDES_raw"];
-            console.log(result_cidoc)
-            res.send({result_cidoc})
-        } catch (e) {
-            console.log(e)
-        }
-
-    } )
-
-    // texts on objects from the collection.
-    app.get('/id/texts/', async(get, res)=> {
-        const _texts = await fetchTexts()
-        const _range=_texts.length
-        const catalogue = [];
-
-        const _context = [
-            "https://apidg.gent.be/opendata/adlib2eventstream/v1/context/cultureel-erfgoed-event-ap.jsonld",
-            "https://apidg.gent.be/opendata/adlib2eventstream/v1/context/cultureel-erfgoed-object-ap.jsonld"
-        ]
-
-        for (let text=0; text<_range; text++) {
-            let _text = {};
-            let catalogueTexts = [];
-            let _objectNumber = _texts[text]["object_number"]
-            let _objectID = "https://data.designmuseumgent.be/objects/"+_objectNumber //todo: add resolver when the object has not been published yet.
-
-            //nl
-            if (_texts[text]["text_NL"]) {
-                const _textNL = {
-                    "text": _texts[text]["text_NL"],
-                    "@lang": "nl"
-                }
-                catalogueTexts.push(_textNL);
-            }
-
-            //en
-            if (_texts[text]["text_EN"]) {
-                const _textEN = {
-                    "text": _texts[text]["text_EN"],
-                    "@lang": "en"
-                }
-                catalogueTexts.push(_textEN);
-            }
-
-            //fr
-            if (_texts[text]["text_FR"]) {
-                const _textFR = {
-                    "text": _texts[text]["text_FR"],
-                    "@lang": "fr"
-                }
-                catalogueTexts.push(_textFR);
-            }
-
-            _text["@context"] = _context
-            _text["@type"] = "InformatieObject";
-            _text["InformatieObject.gaatOver"] = {
-                // todo: or refers to?
-                "@id": _objectID,
-                "@type": "MensgemaaktObject"
-            };
-            _text["InformatieObject.omvat"] = catalogueTexts;
-            catalouge.push(_text);
-        }
-        res.send({catalouge})
-    })
-
+    // ROUTES to texts that are related to the collection of Design Museum Gent
+    requestTexts(app)
 
     console.log("DONE :D :D :D :D ")
 }

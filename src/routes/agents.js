3bf7bcff0e9d239c197af71d0837d5c65f7c39df
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-08-30 11:15:39 +0200

    update agents.

--- src/routes/agents.js
+++ src/routes/agents.js
@@ -4,13 +4,13 @@ export function requestAgents(app) {
     app.get('/id/agents/:agentPID', async(req, res) => {
         const x = await fetchLDESRecordByAgentID(req.params.agentPID);
         const result_cidoc = x[0]["LDES_raw"];
-        res.send({result_cidoc})
+        res.send(x[0]["LDES_raw"])
     })
 
     app.get('/id/ark:/29417/agents/:agentPID', async(req, res) => {
         const x = await fetchLDESRecordByAgentID(req.params.agentPID);
         const result_cidoc = x[0]["LDES_raw"];
-        res.send({result_cidoc})
+        res.send(x[0]["LDES_raw"])
     })
 }
 

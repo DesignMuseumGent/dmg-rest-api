c6556e9cd3d6c9e0df8f49035f2bca361d157883
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-10-12 14:22:10 +0200

    fix bug: type coorecion;

--- src/utils/resolver.js
+++ src/utils/resolver.js
@@ -5,7 +5,7 @@ export function resolver(PURL, _route, res){
     }
     // if _route is not the same as PURL (unhealthy stream) - refetch _route.
     else {
-        if (_route = "https://data.designmuseumgent.be/id/object/UNHEALTHY") {
+        if (_route === "https://data.designmuseumgent.be/id/object/UNHEALTHY") {
             res.status(422).json({"error":"Oops. the syntax of your request is correct, but data on this object has either not yet been published or we are working on repairing this link."})
         } else {
             res.redirect(_route);

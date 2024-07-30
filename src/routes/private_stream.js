e1098dc8a49af8c753ce153544aa1d28402e7570
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-07-30 14:16:54 +0200

    Refactor private stream endpoint for efficiency
    Removed redundant checks and improved key authentication handling by returning early on failure. Simplified limit and offset calculations, and utilized array methods to streamline object fetching and response.

--- src/routes/private_stream.js
+++ src/routes/private_stream.js
@@ -7,52 +7,26 @@ export function requestPrivateObjects(app) {
   app.get("id/private-objects/", async (req, res) => {
     // AUTHENTICATION
     let keys = await fetchAuthentication();
-    console.log(keys);
     // check if the key is correct
     let apiKey = req.query.apiKey || "none";
 
-    if (keys.some((item) => item.key === apiKey)) {
-      // request limit
-      let limit = parseInt(req.query.limit) || 100; // defautlt limit = 10;
-      if (limit > 1000) {
-        // set limit to 1000 items per request.
-        limit = 1000;
-      }
-      let offset = parseInt(req.query.offset) || 0; //
-      let rangeStart, rangeEnd;
-      console.log(offset);
-
-      if (offset == 0) {
-        rangeStart = 0;
-        rangeEnd = limit;
-      } else {
-        rangeStart = limit;
-        rangeEnd = (offset + 1) * limit;
-      }
-
-      console.log(rangeStart, rangeEnd);
-      const x = await fetchAllPrivateLDESrecordsObjects(rangeStart, rangeEnd); // fetch data from supabase based on limit and range
-      const _objects = [];
-
-      // loop over data and write to api
-      for (let i = 0; i < limit; i++) {
-        try {
-          _objects.push(x[i]["LDES_raw"]);
-        } catch (e) {
-          console.error(e);
-        }
-      }
-      res.send(_objects);
-      return;
-    }
-
-    // else send error
-    else {
-      res.status(401).json({
-        error:
-          "authentication key is missing. this stream is only available on request.",
+    if(!keys.some((item) => item.key === apiKey)){
+      return res.status(401).json({
+        error: "Authentication key is missing. this stream is only available via authentication"
       });
-      return;
     }
+
+    let limit = parseInt(req.query.limit) || 10; // default limit = 10;
+    limit = Math.min(limit, 1000); // Set limit to maximum 1000 items per request
+
+    const offset = parseInt(req.query.offset) || 0;
+    const rangeStart = offset === 0 ? 0 : limit;
+    const rangeEnd = (offset + 1) * limit
+
+    const records = await fetchAllPrivateLDESrecordsObjects(rangeStart, rangeEnd)
+    const objects = records.slice(0, limit).map(record=>record["LDES_raw"])
+
+    return res.send(objects)
+
   });
 }

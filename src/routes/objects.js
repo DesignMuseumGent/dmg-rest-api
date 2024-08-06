3840c8896677a25d050aaea5cc8e134dd1c7404d
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-08-06 10:35:20 +0200

    Implement pagination in object fetching API
    Updated the `/v1/id/objects/` endpoint to include pagination support. Added constants for common JSON-LD context and refactored the endpoint to return a paginated response with relevant metadata for ease of API consumption.

--- src/routes/objects.js
+++ src/routes/objects.js
@@ -1,52 +1,60 @@
 import {
   fetchAllLDESrecordsObjects,
-  parseBoolean,
 } from "../utils/parsers.js";
 
-const DEFAULT_OFFSET = 0;
+const COMMON_CONTEXT = [
+  "https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-object/erkendestandaard/2021-04-22/context/cultureel-erfgoed-object-ap.jsonld",
+  "https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-event/erkendestandaard/2021-04-22/context/cultureel-erfgoed-event-ap.jsonld",
+  "https://data.vlaanderen.be/doc/applicatieprofiel/generiek-basis/zonderstatus/2019-07-01/context/generiek-basis.jsonld",
+];
 
 export function requestObjects(app, BASE_URI) {
   app.get("/v1/id/objects/", async (req, res) => {
-    // await data from GET request (supabase)
+
     const records = await fetchAllLDESrecordsObjects();
+    const filteredObjects = [];
 
-    let limit = Math.min(parseInt(req.query.limit) || records.length, records.length); // if no limit set, return all items.
-    let offset = parseInt(req.query.offset) || DEFAULT_OFFSET; // Default offset is 0
-    let idOnly = parseBoolean(req.query.idOnly) || false; // if not idOnly, return all items
-    let collection = req.query.collection || false
+    // pagination
+    let { pageNumber = 1, itemsPerPage = 20 } = req.query
+    pageNumber = Number(pageNumber)
+    itemsPerPage = Number(itemsPerPage)
 
-    const maxOffset = Math.floor(records.length / limit)
-    offset = Math.min(offset, maxOffset)
+    const totalPages = Math.ceil(records.length / itemsPerPage)
 
-    const objects = records.map(record => {
-      if (idOnly) {
-        return record["objectNumber"]
-      }
-      if (collection && record["LDES_raw"]["object"]) {
-        return record["LDES_raw"]["object"];
-      }
+    for (let i = (pageNumber - 1) * itemsPerPage; i < pageNumber * itemsPerPage; i++) {
+      if (i >= records.length) break;
+      const record = records[i];
 
-      let object = {};
-      object["@context"] = [
-        "https://apidg.gent.be/operfgoed-object-ap.jsonld",
-        "https://apidg.gent.be/op…xt/generiek-basis.jsonld",
-      ]
-
-      object["@id"] = BASE_URI + "id/object/" + record["objectNumber"]; // create id (PID) for individual object
-      object["@type"] = "MensgemaaktObject";
-      object["Object.identificator"] = [
-        {
+      let object = {
+        "@context": COMMON_CONTEXT,
+        "@id": `${BASE_URI}id/object/${record["objectNumber"]}`,
+        "@type": "MensgemaaktObject",
+        "Object.identificator": [{
           "@type": "Identificator",
           "Identificator.identificator": {
             "@value": record["objectNumber"],
           },
-        },
-      ];
-      return object;
-    })
+        }],
+      };
 
-    const responseObject = objects.slice(offset, offset + limit);
-    const status = responseObject.length ? 200: 204;
-    res.status(status).json(responseObject)
+      filteredObjects.push(object);
+    }
+
+    res.status(200).json({
+      "@context": [...COMMON_CONTEXT, { "hydra": "http://www.w3.org/ns/hydra/context.jsonld" }],
+      "@type": "GecureerdeCollectie",
+      "@id": `${BASE_URI}id/objects`,
+      "hydra:view": {
+        "@id": `${BASE_URI}id/objects?pageNumber=${pageNumber}`,
+        "@type": "PartialCollectionView",
+        "hydra:first": `${BASE_URI}id/objects?pageNumber=1`,
+        "hydra:last": `${BASE_URI}id/objects?pageNumber=${totalPages}`,
+        "hydra:previous": pageNumber > 1 ? `${BASE_URI}id/objects?pageNumber=${pageNumber - 1}` : null,
+        "hydra:next": pageNumber < totalPages ? `${BASE_URI}id/objects?pageNumber=${pageNumber + 1}` : null,
+      },
+      "GecureerdeCollectie.curator": "Design Museum Gent",
+      "GecureerdeCollectie.bestaatUit": filteredObjects
+    });
+
   })
 }

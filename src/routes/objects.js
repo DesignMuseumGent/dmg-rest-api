a579edda96ad61ae369ed51dba147d6aaddeac69
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-08-06 09:57:39 +0200

    Refactor object routes
    Separated `object` route handler into its own module. This improves code modularity and maintains a clear separation between different API endpoints. Adjusted imports accordingly in `app.js`.

--- src/routes/objects.js
+++ src/routes/objects.js
@@ -1,15 +1,8 @@
 import {
   fetchAllLDESrecordsObjects,
-  fetchLDESRecordByObjectNumber,
   parseBoolean,
 } from "../utils/parsers.js";
-import { resolver } from "../utils/resolver.js";
-import {response} from "express";
 
-// constants
-const ERROR_410 = "the object is permanently removed from our collection";
-const ERROR_422 = "Oops. the syntax of your request is correct, but data on this object has either not yet been published or we are working on repairing this link."
-const DEFAULT_LIMIT = 10;
 const DEFAULT_OFFSET = 0;
 
 export function requestObjects(app, BASE_URI) {
@@ -56,114 +49,4 @@ export function requestObjects(app, BASE_URI) {
     const status = responseObject.length ? 200: 204;
     res.status(status).json(responseObject)
   })
-}
-
-export function requestObject(app, BASE_URI) {
-
-  const objectHandler = async(req, res) => {
-    // 1. resolve to special pages
-    if (req.params.objectNumber === "removed") {
-      return res.status(410).json({
-        410: ERROR_410,
-      });
-    }
-
-    // 2. fetch information
-    // await data from GET request (supabase)
-    const x = await fetchLDESRecordByObjectNumber(req.params.objectNumber);
-    const _redirect =
-        "https://data.collectie.gent/entity/dmg:" + req.params.objectNumber;
-    let _error = "";
-    let _manifest = false;
-    let _open = false;
-    let result_cidoc;
-
-    // define path to resolve to
-    if (x[0]["RESOLVES_TO"]) {
-      let _route = x[0]["RESOLVES_TO"];
-      let _PURL = x[0]["PURI"];
-
-      // resolver
-      if (resolver(_PURL, _route, res, req)) {
-        result_cidoc = x;
-      }
-    }
-
-    function parseBoolean(string) {
-      return string === "true"
-          ? true
-          : string === "false"
-              ? false
-              : undefined;
-    }
-
-    // if asked for image, only return manifest link.
-    try {
-      _manifest = parseBoolean(req.query.manifest) || false;
-      _open = parseBoolean(req.query.open) || false;
-    } catch (e) {
-      console.log(e);
-    }
-
-    try {
-      // redefine - @id to use URIs and PIDs defined by the museum
-      result_cidoc[0]["LDES_raw"]["object"]["@id"] =
-          BASE_URI + "id/object/" +
-          req.params.objectNumber;
-      // assign foaf:pages
-      result_cidoc[0]["LDES_raw"]["object"]["foaf:homepage"] =
-          BASE_URI + "id/object/" +
-          req.params.objectNumber;
-    } catch (error) {
-      _error = error;
-    }
-
-    // error handling.
-    try {
-      if (result_cidoc.length !== 0) {
-        req.negotiate(req.params.format, {
-          json: function () {
-            // if manifest only send manifest;
-            if (_manifest && !_open) {
-              let _man = result_cidoc["object"];
-              res.send(
-                  result_cidoc[0]["LDES_raw"]["object"][
-                      "http://www.cidoc-crm.org/cidoc-crm/P129i_is_subject_of"
-                      ]["@id"],
-              );
-            } else if (_manifest && _open) {
-              res.redirect(
-                  result_cidoc[0]["LDES_raw"]["object"][
-                      "https://www.cidoc-crm.org/cidoc-crm/P129i_is_subject_of"
-                      ]["@id"],
-              );
-            } else {
-              // if format .json redirect to machine-readable page.
-              // res.set('Content-Type', 'application/json+ld;charset=utf-8')
-              res.send(result_cidoc[0]["LDES_raw"]["object"]);
-            }
-            //todo: add route to page when not published yet. -- this object has not been published yet.
-          },
-          html: function () {
-            // if format .html redirect to human-readable page
-            res.redirect(_redirect);
-          },
-          default: function () {
-            //send html anyway.
-            res.redirect(_redirect);
-          },
-        });
-      } else {
-        // string is correct / but object can't be found.
-        res.status(422).json({
-          error:
-          ERROR_422,
-        });
-      }
-    } catch (e) {
-      res.status(503).send(e);
-    }
-  }
-  app.get("/v1/id/object/:objectNumber.:format?",  objectHandler);
-  app.get("/v1/id/ark:/29417/object/:objectNumber.:format?",  objectHandler);
 }

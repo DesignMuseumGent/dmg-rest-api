14ccfa90e3fe378b2e2ebdf8c1038a347640b833
oliviervd <oliviervandhuynslager@gmail.com>
2024-08-28 12:01:55 +0200

    improve response speed, implent async

--- src/routes/curated/colors.js
+++ src/routes/curated/colors.js
@@ -3,9 +3,9 @@
 import {fetchAllLDESrecordsObjects} from "../../utils/parsers.js";
 import {colors_dict} from "../../utils/colors_dict.js";
 
-function addImageUri(filteredObjects, object, BASE_URI) {
+async function addImageUri(filteredObjects, object, BASE_URI) {
     if (object["iiif_image_uris"]) {
-        object["iiif_image_uris"].forEach(uri => {
+        for (const uri of object["iiif_image_uris"]) {
             filteredObjects.push({
                 "@context": [
                     "https://data.vlaanderen.be/doc/applicatieprofiel/cultureel-erfgoed-object/erkendestandaard/2021-04-22/context/cultureel-erfgoed-object-ap.jsonld",
@@ -22,7 +22,7 @@ function addImageUri(filteredObjects, object, BASE_URI) {
                     "cidoc:P1_is_identified_by": `${BASE_URI}id/object/${object["objectNumber"]}`
                 }
             });
-        })
+        }
     }
 }
 
@@ -35,58 +35,52 @@ export function requestByColor(app, BASE_URI) {
     app.get('/v1/color-api/:color', async (req, res) => {
 
         const objects = await fetchAllLDESrecordsObjects();
-        const filteredObjects = [];
         const targetColor = req.params.color.toLowerCase();
-        const fuzzy = req.query.fuzzy || true;
+        const fuzzy = req.query.fuzzy !== "false";
 
         // pagination
         let { page = 1, itemsPerPage =  20 } = req.query;
         page = Number(page)
         itemsPerPage =  Number(itemsPerPage)
 
-        let matchingObjects = [] // first extract the color matching
 
-        for (let object of objects) {
-            const colors = object.color_names || [];
-
-            // use Array.prototype.some to stop iterating when match is found.
-            const isMatchedColor = colors.some(color=>{
-                if (Array.isArray(color)) {
-                    return color.some(item => {
-                        let lowerItem = item.toLowerCase();
-                        let lowerTarget = targetColor.toLowerCase();
-                        return fuzzy ? lowerItem.includes(lowerTarget) : lowerItem === lowerTarget;
-                    });
-                }
-                return targetColor === color.toLowerCase();
-            })
-
-            if (isMatchedColor) {
-                if(req.query.image) {
-                    let potentialObject = addImageUri(matchingObjects, object, BASE_URI);
-                    if(potentialObject !== undefined) {
-                        matchingObjects.push(potentialObject);
-                    }
-                } else {
-                    if(object["LDES_raw"] !== undefined) {
-                        matchingObjects.push(object["LDES_raw"]);
-                    }
-                }
-            }
+        const colorMatchingPromises = objects.map(async object => {
+      const colors = object.color_names || [];
+      const isMatchedColor = colors.some(color => {
+        if (Array.isArray(color)) {
+          return color.some(item => {
+            const lowerItem = item.toLowerCase();
+            const lowerTarget = targetColor.toLowerCase();
+            return fuzzy ? lowerItem.includes(lowerTarget) : lowerItem === lowerTarget;
+          });
         }
+        return targetColor === color.toLowerCase();
+      });
 
-        // apply pagination on matching objects and store result in filteredObjects
-        let startIndex = (page - 1) * itemsPerPage;
-        if (startIndex < matchingObjects.length) {
-            filteredObjects.push(...matchingObjects.slice(startIndex, startIndex + itemsPerPage));
+      if (isMatchedColor) {
+        if (req.query.image) {
+          await addImageUri(matchingObjects, object, BASE_URI);
+        } else if (object["LDES_raw"] !== undefined) {
+          return object["LDES_raw"];
         }
+      }
+      return null;
+    });
 
-        // return INFO HYDRA.
-        let totalPages = Math.ceil(matchingObjects.length / itemsPerPage);
-        const firstPage = 1;
-        const lastPage = totalPages;
-        const previousPage = page > firstPage ? page - 1 : null;
-        const nextPage = page < lastPage ? page + 1 : null;
+    let matchingObjects = (await Promise.all(colorMatchingPromises)).filter(Boolean);
+
+    // Apply pagination and store result in filteredObjects
+    const startIndex = (page - 1) * itemsPerPage;
+    const filteredObjects = startIndex < matchingObjects.length
+      ? matchingObjects.slice(startIndex, startIndex + itemsPerPage)
+      : [];
+
+    // Return INFO HYDRA
+    const totalPages = Math.ceil(matchingObjects.length / itemsPerPage);
+    const firstPage = 1;
+    const lastPage = totalPages;
+    const previousPage = page > firstPage ? page - 1 : null;
+    const nextPage = page < lastPage ? page + 1 : null;
 
         res.status(200).send({
             "@context": [
